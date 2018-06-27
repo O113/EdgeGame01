e6ffaf201ce069888281941412b0c012ef475449
O113 <olle.grek@gmail.com>
2018-06-27 15:46:18 +0200

    s

--- assets/scripts/movement.js
+++ assets/scripts/movement.js
@@ -1,44 +1,39 @@
-var cursors;  
+ 
 
-function createPlayer(){
-cursors = this.input.keyboard.createCursorKeys();
+function createPlayer(object){
 
-this.anims.create ({
+object.anims.create ({
         key: 'left',
-        frames: this.anims.generateFrameNumbers('blocksprite', { start: 0, end: 3}),
+        frames: object.anims.generateFrameNumbers('blocksprite', { start: 0, end: 3}),
         frameRate: 10,
         repeat: -1
         });
-    this.anims.create({
+        object.anims.create({
         key: 'turn',
         frames: [ {key: 'blocksprite', frame: 4 } ],
         frameRate:20
         });
-    this.anims.create({
+        object.anims.create({
         key: 'right',
-        frames: this.anims.generateFrameNumbers('blocksprite', { start: 5, end: 8 } ),
+        frames: object.anims.generateFrameNumbers('blocksprite', { start: 5, end: 8 } ),
         frameRate:10,
         repeat: -1
         });
+
+        object.physics.add.collider(player,platforms);
     };
 
-    this.physics.add.collider(player,platforms);
-
-
-    function checkPlayerMove(){
-if (cursors.left.isDown) {
-    player.setVelocityX(-160);
-
-}
-else if (cursors.right.isDown) {
-    player.setVelocityX(160);
-
-}
-else {
-    player.setVelocityX(0);
-
-}
-if (cursors.up.isDown && player.body.touching.down) {
-    player.setVelocityY(-330);
-}
+function checkPlayerMove(){
+        if (cursors.left.isDown) {
+            player.setVelocityX(-160);
+            }
+            else if (cursors.right.isDown) {
+            player.setVelocityX(160);
+    	    }
+        else {
+        player.setVelocityX(0);
+        }
+    if (cursors.up.isDown && player.body.touching.down) {
+        player.setVelocityY(-200);
+        }
     };

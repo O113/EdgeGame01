98e988c27f089780d3e1b541873ac2dd84aff731
O113 <olle.grek@gmail.com>
2018-06-27 20:34:48 +0200

    moved around code

--- assets/scripts/createLevel.js
+++ assets/scripts/createLevel.js
@@ -1,8 +1,8 @@
-function createLevel1(object){
+function createLevel1(){
 
-    object.create(350,768, 'ground').setScale(1).refreshBody();
-    object.create(700,768, 'ground').setScale(1).refreshBody();
-    object.create(600,400, 'platform');
-    object.create(50,250, 'platform');
-    object.create(750,220, 'platform');
+    platforms.create(350,768, 'ground').setScale(1).refreshBody();
+    platforms.create(700,768, 'ground').setScale(1).refreshBody();
+    platforms.create(600,400, 'platform');
+    platforms.create(50,250, 'platform');
+    platforms.create(750,220, 'platform');
 }

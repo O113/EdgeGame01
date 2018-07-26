c72e26acb27ff058a95347f951a3ab194cb9cc47
O113 <olle.grek@gmail.com>
2018-07-26 13:43:49 +0200

    more ui updates

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -13,6 +13,7 @@ function preload () {
 	this.load.spritesheet('manaBottle', 'assets/art/manaBottle.png', {frameWidth: 100, frameHeight: 190 });
 	this.load.image('hearts', 'assets/hearts.png');
 
+	this.load.image('uiBackground', 'assets/art/uiBackground.png')
 	this.load.image('uiFace', 'assets/art/uiFace.png'); 
 	this.load.image('healthBar', 'assets/art/healthBar.png');
 	this.load.image('manaBar', 'assets/art/manaBar.png');
@@ -29,12 +30,13 @@ function create () {
 
 	particles = this.add.particles('particleWhiteCircle');
 
+	createLevel(0, platforms); 
+
+	this.uiBackground = this.add.image(0, 475, 'uiBackground');
 	this.uiFace = this.add.image(0, 475, 'uiFace'); 
 	this.healthBar = this.add.image(150, 450, 'healthBar'); 
 	this.staminaBar = this.add.image(150, 475, 'staminaBar'); 
 	this.manaBar = this.add.image(150, 500, 'manaBar'); 
-	
-	createLevel(0, platforms); 
 
 	createPlayer(this, platforms); 
 
@@ -51,10 +53,11 @@ function update () {
 	this.player.currManaScale = this.player.currMana/this.player.maximumMana;
 	this.player.currStaminaScale = this.player.currStamina/this.player.maximumStamina; 
 
-	this.uiFace.setPosition(this.player.x-400,this.player.y-235);
-	this.healthBar.setPosition(this.player.x-350 +(this.player.currHPScale * 100),this.player.y-260).setScale(this.player.currHPScale, 1);
-	this.staminaBar.setPosition(this.player.x-350+(this.player.currStaminaScale * 100),this.player.y-235).setScale(this.player.currStaminaScale, 1);
-	this.manaBar.setPosition(this.player.x-350 +(this.player.currManaScale * 100),this.player.y-210).setScale(this.player.currManaScale, 1);
+	this.uiBackground.setPosition(this.player.x-350,this.player.y-275);
+	this.uiFace.setPosition(this.player.x-460,this.player.y-275);
+	this.healthBar.setPosition(this.player.x-415 +(this.player.currHPScale * 100),this.player.y-300).setScale(this.player.currHPScale, 1);
+	this.staminaBar.setPosition(this.player.x-415+(this.player.currStaminaScale * 100),this.player.y-275).setScale(this.player.currStaminaScale, 1);
+	this.manaBar.setPosition(this.player.x-415 +(this.player.currManaScale * 100),this.player.y-250).setScale(this.player.currManaScale, 1);
 
 	if(this.player.currStamina < this.player.maximumStamina)
 	{
